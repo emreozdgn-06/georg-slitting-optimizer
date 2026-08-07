13068849887c32e61af72646ef5e985a8400ddac
emreozdgn-06 <emreozdgn06@gmail.com>
2026-08-07 10:36:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -99,19 +99,22 @@ with col2:
         df_coil = pd.read_excel(coil_dosyasi)
 
         st.dataframe(df_coil, use_container_width=True)
-st.write("### Mother Coil Bilgileri")
 
-st.write(f"Toplam Coil : {len(df_coil)}")
+        st.write("### Mother Coil Bilgileri")
 
-st.write("Kolonlar")
+        st.write(f"Toplam Coil : {len(df_coil)}")
 
-st.write(list(df_coil.columns))
-st.write("### Mother Coil Listesi")
+        st.write("Kolonlar")
 
-st.dataframe(
-    df_coil,
-    use_container_width=True
-)
+        st.write(list(df_coil.columns))
+
+        st.write("### Mother Coil Listesi")
+
+        st.dataframe(
+            df_coil,
+            use_container_width=True
+        )
+
 st.divider()
 
 # -----------------------------
